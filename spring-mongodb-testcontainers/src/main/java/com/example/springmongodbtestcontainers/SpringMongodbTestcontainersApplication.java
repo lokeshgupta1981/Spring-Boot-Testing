f867029aeaf2cf24afcb37726a7a6c65b7c2f772
Abhinav Pandey <abhinavpandey312@gmail.com>
2023-01-12 19:59:13 +0530

    Spring Boot testing with Testcontainers

---
+++ spring-mongodb-testcontainers/src/main/java/com/example/springmongodbtestcontainers/SpringMongodbTestcontainersApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmongodbtestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbTestcontainersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMongodbTestcontainersApplication.class, args);
+    }
+
+}
---
+++ spring-mongodb-testcontainers/src/main/java/com/example/springmongodbtestcontainers/entity/Product.java
@@ -0,0 +1,45 @@
+package com.example.springmongodbtestcontainers.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document
+public class Product implements Serializable {
+    private String id;
+    private String name;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
